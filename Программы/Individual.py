c3b932a42ebb7a8b992dfdf3447092bec2ba70a8
AlexRepkin <realpal30@gmail.com>
2024-05-24 09:29:49 +0300

    Everything's done!

---
+++ Программы/Individual.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Manager
+
+
+'''Варианты №3 и №2(По списку 28)
+Для своего индивидуального задания лабораторной работы 2.23 необходимо реализовать
+вычисление значений в двух функциях в отдельных процессах.
+
+Индивидуальное задание из работы 2.23:
+С использованием многопоточности для x = 0.7(Во 2) и x = 1.2 (В 3),
+находится сумма ряда S с точностью члена ряда по абсолютному
+значению ε = 10e^(-7) и производится сравнение полученной суммы с контрольным
+значением функции y = 1/(1-x) (Во 2) и y = 1/(2+x) (В 3) для двух бесконечных рядов'''
+
+
+def second_var(results):
+    # Вычисление суммы ряда для x = 0.7
+    s = 0
+    n = 0
+    while True:
+        element = 0.7**n
+        if element < 1e-7:  # Проверка условия остановки (ε).
+            break
+        else:
+            s += element
+            n += 1
+    results["second"] = s
+
+
+def third_var(results):
+    # Вычисление суммы ряда для x = 1.2
+    s = 0
+    n = 0
+    while True:
+        element = ((-1)**n)*(1.2**n)/2**(n+1)
+        if abs(element) < 1e-7:  # Проверка условия остановки (ε).
+            break
+        else:
+            s += element
+            n += 1
+    results["third"] = s
+
+
+def main():
+    # Создание менеджера контекста, гарантирующего, что ресурсы будут освобождены по завершении блока with
+    with Manager() as manager:
+        # Создание разделяемого словаря, используемого для обмена данными между процессами.
+        results = manager.dict()
+        # По заданию, y во втором варианте = 1/(1-x). Так как x = 0.7, то y = 10/3
+        # y в третьем варианте = 1/(2+x). Так как x = 1.2, то y = 1/(2+1.2) = 0.3125
+        y1 = 10/3
+        y2 = 0.3125
+
+        # Создание процессов для вычисления сумм.
+        first_thread = Process(target=second_var, args=(results,))
+        second_thread = Process(target=third_var, args=(results,))
+
+        # Запуск созданных потоков.
+        first_thread.start()
+        second_thread.start()
+        # Блокировка основного потока, пока эти два потока не завершатся.
+        first_thread.join()
+        second_thread.join()
+
+        print("Good day! According to our calculations:\nSum of elements in variant 2 =",
+              results["second"])
+        print("At the same time, y =", y1, "/n")
+        print("For the 3 variant, sum of elements =", results["third"])
+        print("y =", y2)
+
+
+if __name__ == "__main__":
+    main()
